bf728c72ba30200d8dd6d6c5f83b5cbe2c921109
sps169 <sps169@outlook.com>
2024-08-04 14:02:35 +0200

    refactor

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: sperez-s <sperez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 17:42:08 by sperez-s          #+#    #+#             */
-/*   Updated: 2024/08/02 17:49:49 by sperez-s         ###   ########.fr       */
+/*   Updated: 2024/08/04 14:00:41 by sperez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,39 @@ static size_t	get_str_len(char const *s)
 	return (len);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static char	*do_join(size_t s1_s, size_t s2_s, char const *s1, char const *s2)
 {
-	char			*result;
-	size_t			s1_size;
-	size_t			s2_size;
 	unsigned int	i;
+	char			*result;
 
-	if (!s1 || !s2)
-		return (NULL);
-	s1_size = get_str_len(s1);
-	s2_size = get_str_len(s2);
-	result = malloc((s1_size + s2_size + 1) * sizeof(char));
+	i = 0;
+	result = NULL;
+	result = malloc((s1_s + s2_s + 1) * sizeof(char));
 	if (result != NULL)
 	{
-		i = 0;
-		while (i < s1_size)
+		while (i < s1_s)
 		{
 			result[i] = s1[i];
 			i++;
 		}
-		while (i < s1_size + s2_size)
+		while (i < s1_s + s2_s)
 		{
-			result[i] = s2[i - s1_size];
+			result[i] = s2[i - s1_s];
 			i++;
 		}
 		result[i] = 0;
 	}
 	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t			s1_size;
+	size_t			s2_size;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_size = get_str_len(s1);
+	s2_size = get_str_len(s2);
+	return (do_join(s1_size, s2_size, s1, s2));
 }
